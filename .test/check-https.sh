30b21b2678725d52a50f2aa6b67b285b0e05b9a8
Curtis Vogt <curtis.vogt@gmail.com>
2017-12-18 11:46:47 -0600

    Verify HTTPS URLs end with .git

--- .test/check-https.sh
+++ .test/check-https.sh
@@ -3,10 +3,21 @@
 # Ensure that URLs with the specified host must use HTTPS without a username and/or password.
 host=$1
 
-cd $(dirname $0)/..  # `git diff` and `xargs` need to in the correct directory
-if git diff origin/invenia HEAD -G $host -- "*/url" | grep $host | grep -qv https://$host; then
-    echo "Registered package URLs to $host must use HTTPS"
-    exit 1
-else
-    exit 0
-fi
+cd $(dirname $0)/..  # `git diff` needs run to in the correct directory
+
+# Look only at URL additions which contain the hostname
+urls=($(git diff origin/invenia HEAD -G $host -- "*/url" | grep -E '^\+' | grep $host | sed -e 's/^\+//'))
+
+for url in "${urls[@]}"; do
+    if echo $url | grep -qv https://$host; then
+        echo "Registered package URLs to \"$host\" must use HTTPS. Invalid URL: \"$url\""
+        exit 1
+    fi
+
+    if echo $url | grep -qvE '\.git$'; then
+        echo "Registered package URLs using HTTP must end with \".git\". Invalid URL: \"$url\""
+        exit 1
+    fi
+done
+
+exit 0
